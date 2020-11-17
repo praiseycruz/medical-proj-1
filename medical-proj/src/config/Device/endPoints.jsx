1aa1ffad997697c925289ce41927ec149987c37a
Mythreya K <medtrack.one@gmail.com>
2020-11-17 13:28:37 +0530

    Fetch devices that we created

--- medical-proj/src/config/Device/endPoints.jsx
+++ medical-proj/src/config/Device/endPoints.jsx
@@ -2,5 +2,5 @@ export const endPoints = {
     create: '/Device',
     findById: (id) => `/Device/${id}?_pretty=false`,
     assignPatient: (deviceId) => `/Device/${deviceId}?_pretty=false`,
-    findUnassigned: "Device?patient:missing=true&_pretty=false"
+    findUnassigned: "Device?identifier=EXSYS|&patient:missing=true&_pretty=false"
 }
