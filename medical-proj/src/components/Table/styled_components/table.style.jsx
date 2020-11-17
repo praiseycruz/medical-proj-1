dc9630958f730192a6d5c995321ca1d41aeeedf5
Praisey Cruz <56293074+advallu-praisey@users.noreply.github.com>
2020-11-17 13:50:56 +0800

    updated table component

--- medical-proj/src/components/Table/styled_components/table.style.jsx
+++ medical-proj/src/components/Table/styled_components/table.style.jsx
@@ -2,18 +2,52 @@ import styled from 'styled-components'
 
 export const TableWrapper = styled.div`
     table {
-        td, th {
-            color: #949ba2;
-            padding: 15px;
-            font-size: 14px;
-            border-top: 1px solid #4f5467;
+        border-collapse: collapse;
+        border-spacing: 0;
+        border-collapse: separate;
+        border-spacing: 0;
+        color: #4a4a4d;
+        font-size: 14px;
+        width: 100%;
+
+        tr {
+            overflow-x: scroll;
+        }
+
+        th,
+        td {
+            padding: 15px 15px;
+            vertical-align: middle;
         }
 
         thead {
-            border-top: 1px solid #4f5467;
+            font-size: 14px;
+            line-height: 24px;
+            border: 1px solid transparent;
+            max-width: 100%;
+            font-weight: 900;
+            line-height: 24px;
+            mix-blend-mode: normal;
+            color: #ddd;
+            background-color: #333a46;
+        }
+
+        thead {
+            tr {
+                th {
+                    padding: 15px 15px;
+                    border-bottom: 1px solid #ffffff;
+
+                    &:last-child {
+                        text-align: center;
+                    }
+                }
+            }
         }
 
         tbody {
+            max-width: 100%;
+
             tr {
                 td {
                     &.devices {
@@ -40,8 +74,41 @@ export const TableWrapper = styled.div`
                             padding: 5px 20px;
                         }
                     }
+
+                    &:last-child {
+                        text-align: center;
+                    }
                 }
             }
         }
+
+        td {
+            padding: 15px 15px;
+            border-bottom: 1px solid #424955;
+            color: #949ba2;
+            text-transform: capitalize;
+
+            .edit {
+                font-size: 12px;
+                padding: 4px 12px;
+            }
+        }
+    }
+
+    .spinner-wrapper {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 20px;
+        color: #fff;
+    }
+
+    .no-records {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-top: 2px solid #fff;
+        border-bottom: 1px solid #fff;
+        padding: 15px;
     }
 `
--- medical-proj/src/components/Table/table.component.jsx
+++ medical-proj/src/components/Table/table.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Table } from 'react-bootstrap'
+import { Table, Spinner } from 'react-bootstrap'
 import { TableWrapper } from './styled_components/table.style'
 
 export class TableComponent extends React.Component {
@@ -8,33 +8,90 @@ export class TableComponent extends React.Component {
     }
 
     render() {
-        let { data, cols, bordered, striped, removeThead, isTableFor } = this.props
+        let { data, cols, bordered, striped, removeThead, isTableFor, loading, total } = this.props
 
         return (
             <TableWrapper>
-                <Table
-                    responsive
-                    hover
-                    size="sm"
-                    className={`${bordered ? 'table-bordered' : 'table-borderless'} ${striped ? 'table-striped' : ''}`}>
-                    <thead className={`${removeThead ? 'd-none' : ''}`}>
-                        <tr>
-                            {cols.map((headerItem, index) => (
-                                <th key={index}>{headerItem.title}</th>
-                            ))}
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                        {data.map((item, index) => (
-                            <tr key={index}>
-                                {cols.map((col, key) => (
-                                    <td key={key} className={isTableFor}>{col.render(item)}</td>
+                {/* loading !== 'undefined' && loading !== null && !loading ?
+                    <Table
+                        responsive
+                        hover
+                        size="sm"
+                        className={`${bordered ? 'table-bordered' : 'table-borderless'} ${striped ? 'table-striped' : ''}`}>
+                        <thead className={`${removeThead ? 'd-none' : ''}`}>
+                            <tr>
+                                {cols.map((headerItem, index) => (
+                                    <th key={index}>{headerItem.title}</th>
                                 ))}
                             </tr>
-                        ))}
-                    </tbody>
-                </Table>
+                        </thead>
+
+                        <tbody>
+                            { data.map((item, index) => (
+                                <tr key={index}>
+                                    {cols.map((col, key) => (
+                                        <td key={key} className={isTableFor}>{col.render(item)}</td>
+                                    ))}
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+                    :
+                    <div className="spinner-wrapper">
+                        <Spinner animation="border" role="status">
+                            <span className="sr-only">Loading...</span>
+                        </Spinner>
+                        <span className="ml-3">Loading data...</span>
+                    </div>
+                */}
+
+                { loading !== 'undefined' && loading !== null && !loading ?
+                    <>
+                        { data.length > 0 ?
+                            <Table
+                                responsive
+                                hover
+                                size="sm"
+                                className={`${bordered ? 'table-bordered' : 'table-borderless'} ${striped ? 'table-striped' : ''}`}>
+                                <thead className={`${removeThead ? 'd-none' : ''}`}>
+                                    <tr>
+                                        {cols.map((headerItem, index) => (
+                                            <th key={index}>{headerItem.title}</th>
+                                        ))}
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                    { data.map((item, index) => (
+                                        <tr key={index}>
+                                            {cols.map((col, key) => (
+                                                <td key={key} className={isTableFor}>{col.render(item)}</td>
+                                            ))}
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </Table> :
+                            <>
+                                { total == 0 &&
+                                    <div className="no-records">No records found</div>
+                                }
+                            </>
+                        }
+                    </> :
+                    <div className="spinner-wrapper">
+                        <Spinner animation="border" role="status">
+                            <span className="sr-only">Loading...</span>
+                        </Spinner>
+                        <span className="ml-3">Loading data...</span>
+                    </div>
+                }
+
+                { /*total !== 'undefined' && total !== null && total !== 0 ?
+                    <>
+                        asdasd
+                    </>
+                    : <>No data</>
+                */}
             </TableWrapper>
         )
     }
