865b5cc22f024996ec95d357676c5f787c0fffc5
Praisey Cruz <praiseycruz@gmail.com>
2020-11-25 19:12:54 +0800

    import constant and reducer

--- medical-proj/src/reducers/index.js
+++ medical-proj/src/reducers/index.js
@@ -3,12 +3,14 @@ import { patient } from './patient.reducer';
 import { practitioner } from './practitioner.reducer';
 import { dashboardPatientCount } from './dashboard.reducer';
 import { device } from './device.reducer';
+import { breadCrumbs } from './breadcrumbs.reducer';
 
 const rootReducer = combineReducers({
     patient,
     practitioner,
     dashboardPatientCount,
-    device
+    device,
+    breadCrumbs
 })
 
 export default rootReducer;
