91918fbe13acc9baa112672987634968ea339560
Kenneth John Rosales <kenjos75@yahoo.com>
2020-12-01 18:50:32 +0800

    Added modal search styled component.

---
+++ medical-proj/src/components/Modals/styled-components/ModalSearchStyle.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const ModalSearchStyle = styled.div`
+`
+
+export const ModalSearchItems = styled.div `
+
+	cursor: pointer;
+	border-radius: 2rem;
+    padding-left: 1rem;
+    
+	&:hover {
+		background: #282e38;
+	}
+
+`
